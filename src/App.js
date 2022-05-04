5f0ab2e8ea6ab538305391e5c92e964aed55918e
MonMaramba <monmaramba@yahoo.com>
2022-05-03 17:18:26 -0700

    mapped notes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from './notes';
 
 import './main.scss';
 
@@ -8,7 +9,9 @@ const App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return <Note key={note.id} title={note.title} content={note.content} />;
+      })}
       <Footer />
     </div>
   );
